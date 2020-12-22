1a4620268ab35cbee011bcb77d8bb6122caca46e
Phillip Choi <jaeja-phil@gmail.com>
2020-12-22 23:41:05 +0900

    question setup

---
+++ EvaluateDivision.google.js
@@ -0,0 +1,18 @@
+const solution = (equations, values, queries) => {
+  //
+};
+
+const result = solution(
+  [
+    ["a", "b"],
+    ["b", "c"],
+  ],
+  [2.0, 3.0],
+  [
+    ["a", "c"],
+    ["b", "a"],
+    ["a", "e"],
+    ["a", "a"],
+    ["x", "x"],
+  ]
+);
